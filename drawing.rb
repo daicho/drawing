98c2c37a79d780a5dcc7f40287401621707f295c
daicho <daichimax78@gmail.com>
2020-02-16 00:11:47 +0900

    Rubyファイルを一つに統合

--- drawing.rb
+++ drawing.rb
@@ -1,5 +1,6 @@
 require 'cgi'
 require 'sinatra'
+require 'digest/md5'
 require 'active_record'
 
 # 各種設定
@@ -9,11 +10,16 @@ NAME_MAX = 32  # 名前の最大文字数
 TEXT_MAX = 512 # 書き込みの最大文字数
 
 set :environment, :production
+set :sessions,
+    expire_after: 7200,
+    secret: 'abcdefghij0123456789'
 
 ActiveRecord::Base.configurations = YAML.load_file('database.yml')
 ActiveRecord::Base.establish_connection :development
 
-# データベースにアクセスするためのオブジェクト
+class Account < ActiveRecord::Base
+end
+
 class Post < ActiveRecord::Base
 end
 
@@ -124,4 +130,93 @@ delete '/del' do
     post = Post.find(params[:id])
     post.destroy
     redirect '/'
+end
+
+# 新規登録
+get '/signup' do
+    username = "idaten"
+    rawpasswd = "tokyo2020"
+    algorithm = "1"
+    r = Random.new
+    salt = Digest::MD5.hexdigest(r.bytes(20))
+    hashed = Digest::MD5.hexdigest(salt + rawpasswd)
+
+    puts "salt = #{salt}"
+    puts "username = #{username}"
+    puts "raw password = #{rawpasswd}"
+    puts "algorithm = #{algorithm}"
+    puts "hashed password = #{hashed}"
+
+    # Update database
+    s = Account.new
+    s.id = username
+    s.salt = salt
+    s.hashed = hashed
+    s.algo = algorithm
+    s.save
+
+    # Display all entries in database
+    @s = Account.all
+    @s.each do |a|
+        puts a.id + "\t" + a.salt + "\t" + a.hashed + "\t" + a.algo
+    end
+end
+
+# ログイン
+get '/login' do
+    erb :loginscr
+end
+
+# 認証
+post '/auth' do
+    username = params[:uname]
+    passwd = params[:pass]
+
+    if Account.exists?(username)
+        a = Account.find(username)
+        db_username = a.id
+        db_salt = a.salt
+        db_hashed = a.hashed
+        db_algo = a.algo
+
+        if db_algo == "1"
+            trial_hashed = Digest::MD5.hexdigest(db_salt + passwd)
+        else
+            puts "Unknown algorithm is userd for user #{username}."
+            exit(-2)
+        end
+
+        if db_hashed == trial_hashed
+            session[:login_flag] = true
+            session[:testdata] = "Is this a holdup?"
+            redirect '/contentspace'
+        else
+            session[:login_flag] = false
+            redirect '/failure'
+        end
+    else
+        session[:login_flag] = false
+        redirect '/failure'
+    end
+end
+
+# ログイン失敗
+get '/failure' do
+    erb :failure
+end
+
+# コンテンツページ
+get '/contentspace' do
+    if session[:login_flag] == true
+        @a = session[:testdata]
+        erb :contents
+    else
+        erb :badrequest
+    end
+end
+
+# ログアウト
+get '/logout' do
+    session.clear
+    erb :logout
 end
